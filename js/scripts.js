c66f84cf12d1baf8d94ec7ae371e434153606811
Benjamin <e>
2014-11-06 17:23:54 -0500

    Added psudo filter

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,25 @@ function convFormula(inputLoc){
 var Formula = function(formula){
 	this.propForm = formula;
 }
-
+//It seems for more complex predicates like (A|B)^C
+//The collection of (A|B) as an isolated term is not
+//regarded. I think your code ignores the ()'s   
 Formula.prototype.generalize = function(){
 	var formTokens = this.propForm.split(' ');
 	var parsedForm = "";
+	//perhaps filter the elements and the ^ V || -> <-> 
+	//seperate lists of them
+	var operators = /*list of Strings/charactors ^ || V */  
+	var operands = /* list of Strings IN ORDER */ 
+	for(var i = 0; i< formTokens.length; i++){
+		if(formTokens[i] == '^' || formTokens[i] == '||' || 
+			formTokens[i] == 'v' || formTokens[i] == 'V' || 
+			formTokens[i] == '&&' || formTokens[i] == '->' ||
+			formTokens[i] == '<->' || formTokens[i] == '(+)')
+			operators += formTokens[i]
+		else operands+= formTokens[i]
+	}
+	
 
 	//replace user entered characters with reference keywords
 	for(var i = 0; i < formTokens.length; i++){
